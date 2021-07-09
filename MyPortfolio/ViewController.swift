074233129fca18c6e1cc99c84eabe56f2a09cbf0
Karthik <karthik.samy@anywhere.co>
2021-07-09 14:18:28 +0530

    Initial Commit

---
+++ MyPortfolio/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyPortfolio
+//
+//  Created by Karthik on 09/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
